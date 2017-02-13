f7089f38a3c8289f1fef261a8bc379e999e9cb9a
mike_1966 <mike_1966@163.com>
2017-02-13 09:34:27 +0800

    First commit.

---
+++ app/src/main/java/cn/com/farsgiht/bluetoothdemo/UI/PageIndicatorView.java
@@ -0,0 +1,31 @@
+package cn.com.farsgiht.bluetoothdemo.UI;
+
+import cn.com.farsgiht.bluetoothdemo.R;
+import cn.com.farsgiht.bluetoothdemo.R.drawable;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class PageIndicatorView extends ImageView {
+	private final String TAG = "PageIndicatorView";
+	private LinearLayout mPageIndicLayout;
+
+	public PageIndicatorView(Context context) {
+		super(context);
+		setSelectedView(false);
+	}
+	
+	public void setSelectedView(boolean selected){
+		Bitmap bitmap;
+		if(selected){
+			bitmap = BitmapFactory.decodeResource(getResources(),
+				R.drawable.page_select);
+		}else{
+			bitmap = BitmapFactory.decodeResource(getResources(),
+					R.drawable.page_item);
+		}
+		this.setImageBitmap(bitmap);
+	}
+}
